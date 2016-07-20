df6ab787350dbf20fc293b749fdde74302d3cd19
Rob McDonald <rob@robmcdonald.net>
2016-07-19 22:12:30 -0400

    Set Up Carousel
    Setting up a bootstrap slideshow carousel.

--- home.php
+++ home.php
@@ -9,29 +9,42 @@
               <h1><?php wp_title(''); ?></h1>
             </div>
 
+            <?php
+
+              $args = array(
+                'post_type'     => 'post',
+                'category_name' => 'featured'
+              );
+              $the_query = new WP_Query( $args );
+
+
+            ?>
+
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
               <ol class="carousel-indicators">
-              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-              
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <li data-target="#carousel-example-generic" data-slide-to="<?php echo $the_query->current_post; ?>" class="<?php if( $the_query->current_post == 0 ):?>active<?php endif; ?>"></li>
+
+                <?php endwhile; endif; ?>
               </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-              <div class="item active">
-                <img src="..." alt="...">
-                <div class="carousel-caption">
-                  ...
+              <?php rewind_posts(); ?>
+
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <div class="item active">
+                  <img src="" alt="">
+                  <div class="carousel-caption"><?php the_title(); ?></div>
                 </div>
+
+                <?php endwhile; endif; ?>
+              
               </div>
-              <div class="item">
-                <img src="..." alt="...">
-                <div class="carousel-caption">
-                  ...
-                </div>
-              </div>
-              ...
-            </div>
 
             <!-- Controls -->
             <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
